0060aec696c28aac7294e08ca2aa38bc4cb006ae
Akshansh <akshanshsingh00@gmail.com>
2026-02-08 22:13:31 +0530

    Initial commit

---
+++ src/test/java/com/akshansh/taskmanagementplatform/TaskManagementPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshansh.taskmanagementplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
