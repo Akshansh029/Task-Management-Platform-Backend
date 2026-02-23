0183fc4a30e122082c8fcc8487a7d6d46f10d543
Akshansh <akshanshsingh00@gmail.com>
2026-02-23 20:14:33 +0530

    Customized CommentController

--- src/main/java/com/akshansh/taskmanagementplatform/controller/CommentController.java
+++ src/main/java/com/akshansh/taskmanagementplatform/controller/CommentController.java
@@ -4,6 +4,11 @@ import com.akshansh.taskmanagementplatform.dto.request.CreateCommentRequest;
 import com.akshansh.taskmanagementplatform.dto.request.UpdateCommentRequest;
 import com.akshansh.taskmanagementplatform.dto.response.CommentResponse;
 import com.akshansh.taskmanagementplatform.service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -14,7 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping
-@Tag(name = "User Management", description = "APIs for managing comments")
+@Tag(name = "Comment Management", description = "APIs for managing comments")
 public class CommentController {
     private final CommentService commentService;
 
@@ -22,6 +27,18 @@ public class CommentController {
         this.commentService = commentService;
     }
 
+
+    @Operation(summary = "Create a new comment", description = "Add a new comment to the system")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Comment created successfully",
+                    content = @Content(schema = @Schema(implementation = CommentResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request data",
+                    content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "403", description = "Unauthorized action",
+                    content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "404", description = "User/Project/Task not found",
+                    content = @Content(schema = @Schema()))
+    })
     @PostMapping("/api/tasks/{taskId}/comments")
     public ResponseEntity<CommentResponse> createComment(
             @PathVariable Long taskId,
@@ -31,6 +48,14 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 
+
+    @Operation(summary = "Get all comments", description = "Retrieve a list of all comments in the system")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Comment retrieved successfully",
+                    content = @Content(schema = @Schema(implementation = CommentResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Task not found",
+                    content = @Content(schema = @Schema()))
+    })
     @GetMapping("/api/tasks/{taskId}/comments")
     public ResponseEntity<List<CommentResponse>> getAllComments(
             @PathVariable Long taskId
@@ -39,6 +64,16 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.OK).body(comments);
     }
 
+
+    @Operation(summary = "Update a comment", description = "Update an existing comment's content")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Comment updated successfully",
+                    content = @Content(schema = @Schema(implementation = CommentResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request data",
+                    content = @Content(schema = @Schema())),
+            @ApiResponse(responseCode = "403", description = "Unauthorized action",
+                    content = @Content(schema = @Schema()))
+    })
     @PutMapping("/api/comments/{commentId}")
     public ResponseEntity<CommentResponse> updateComment(
             @PathVariable Long commentId,
@@ -48,6 +83,13 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
 
+
+    @Operation(summary = "Delete a comment", description = "Delete a comment from the system using its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Comment deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Comment not found",
+                    content = @Content(schema = @Schema()))
+    })
     @DeleteMapping("/api/comments/{commentId}")
     public ResponseEntity<Void> deleteComment(
             @RequestHeader Long userId,
