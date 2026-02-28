a8453bd26d364abe8d7371e4003c1bd42024fdc8
Akshansh <akshanshsingh00@gmail.com>
2026-02-28 19:33:05 +0530

    API docs for AuthController

--- src/main/java/com/akshansh/taskmanagementplatform/controller/AuthController.java
+++ src/main/java/com/akshansh/taskmanagementplatform/controller/AuthController.java
@@ -4,10 +4,16 @@ import com.akshansh.taskmanagementplatform.dto.request.CreateUserRequest;
 import com.akshansh.taskmanagementplatform.dto.request.LoginRequest;
 import com.akshansh.taskmanagementplatform.dto.response.LoginResponse;
 import com.akshansh.taskmanagementplatform.dto.response.UserProfileResponse;
-import com.akshansh.taskmanagementplatform.repository.UserRepository;
 import com.akshansh.taskmanagementplatform.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +21,16 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth Controller", description = "APIs for authentication")
 public class AuthController {
     private final AuthService authService;
 
+
+    @Operation(summary = "Register the user", description = "Register the user and add details in database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User registered successfully",
+                    content = @Content(schema = @Schema(implementation = UserProfileResponse.class)))
+    })
     @PostMapping("/register")
     public ResponseEntity<UserProfileResponse> registerUser(
             @Valid @RequestBody CreateUserRequest request
@@ -26,6 +39,12 @@ public class AuthController {
         return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
     }
 
+
+    @Operation(summary = "Login the user", description = "Sign in the user and generate JWT token")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Login successfully",
+                    content = @Content(schema = @Schema(implementation = LoginResponse.class)))
+    })
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> loginUser(
             @Valid @RequestBody LoginRequest request
