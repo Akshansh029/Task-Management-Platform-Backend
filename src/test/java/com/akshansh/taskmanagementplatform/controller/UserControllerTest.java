ad05c7aad7380038d99a642f9ab01a318549a4ea
Akshansh <akshanshsingh00@gmail.com>
2026-04-02 13:37:21 +0530

    Added tests for createUser API in controller

--- src/test/java/com/akshansh/taskmanagementplatform/controller/UserControllerTest.java
+++ src/test/java/com/akshansh/taskmanagementplatform/controller/UserControllerTest.java
@@ -1,6 +1,6 @@
 package com.akshansh.taskmanagementplatform.controller;
 
-import com.akshansh.taskmanagementplatform.config.TestSecurityConfig;
+import com.akshansh.taskmanagementplatform.dto.request.CreateUserRequest;
 import com.akshansh.taskmanagementplatform.dto.response.UserProfileResponse;
 import com.akshansh.taskmanagementplatform.entity.UserRole;
 import com.akshansh.taskmanagementplatform.exception.ResourceNotFoundException;
@@ -13,17 +13,20 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import tools.jackson.databind.ObjectMapper;
 
 import java.time.LocalDateTime;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -32,6 +35,9 @@ class UserControllerTest {
 
     @Autowired
     private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockitoBean
     private UserService userService;
@@ -46,6 +52,7 @@ class UserControllerTest {
     private HttpSecurity httpSecurity;
 
     private MockMvc mockMvc;
+    private UserProfileResponse response;
 
     @BeforeEach
     void setup() {
@@ -54,6 +61,15 @@ class UserControllerTest {
                 .defaultRequest(
                         get("/").servletPath("")        // clears any default
                 )
+                .build();
+
+        this.response = UserProfileResponse.builder()
+                .id(1L)
+                .name("John Doe")
+                .email("johndoe1234@gmail.com")
+                .role(UserRole.MEMBER)
+                .createdAt(LocalDateTime.now())
+                .ownedProjectsCount(2)
                 .build();
     }
 
@@ -65,15 +81,6 @@ class UserControllerTest {
         @DisplayName("Should return 200 OK when Id exists")
         @WithMockUser
         void getUserProfileById_shouldReturn200_whenIdExists() throws Exception {
-            UserProfileResponse response = UserProfileResponse.builder()
-                    .id(1L)
-                    .name("John Doe")
-                    .email("johndoe1234@gmail.com")
-                    .role(UserRole.MEMBER)
-                    .createdAt(LocalDateTime.now())
-                    .ownedProjectsCount(2)
-                    .build();
-
             when(userService.getUserProfileById(1L)).thenReturn(response);
 
             mockMvc.perform(get("/users/1"))
@@ -91,6 +98,52 @@ class UserControllerTest {
 
             mockMvc.perform(get("/users/99"))
                     .andExpect(status().isNotFound());
+        }
+    }
+
+    @Nested
+    @DisplayName("Create User Tests")
+    class CreateUserTests{
+
+        @Test
+        @DisplayName("Should return 201 CREATED when request data is valid")
+        void createUser_shouldReturn201_whenRequestIsValid() throws Exception {
+            CreateUserRequest request = CreateUserRequest.builder()
+                    .name("John Doe")
+                    .email("johndoe1234@gmail.com")
+                    .password("jd123456")
+                    .role(UserRole.MEMBER)
+                    .build();
+
+            when(userService.createUser(request)).thenReturn(response);
+
+            mockMvc.perform(post("/users")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request))
+                    )
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.name").value("John Doe"))
+                    .andExpect(jsonPath("$.email").value("johndoe1234@gmail.com"));
+        }
+
+        @Test
+        @DisplayName("Should return 400 INVALID when request data is invalid")
+        void createUser_shouldReturn400_whenRequestIsInvalid() throws Exception{
+            CreateUserRequest request = CreateUserRequest.builder()
+                    .name("Ja")
+                    .email("johndoe1234@gmail.com")
+                    .role(UserRole.MEMBER)
+                    .password("jd123456")
+                    .build();
+
+
+            when(userService.createUser(request)).thenReturn(response);
+
+            mockMvc.perform(post("/users")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)
+                            ))
+                    .andExpect(status().is4xxClientError());
         }
     }
 }
